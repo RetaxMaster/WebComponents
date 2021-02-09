999ea67024f6dfd2ecb15c82c227bf7fd7fd8fc0
RetaxMaster <retaxmaster@hotmail.com>
2021-02-09 15:50:32 -0600

    Adicion del multislot

--- Slot/my-element.js
+++ Slot/my-element.js
@@ -32,9 +32,17 @@ class myElement extends HTMLElement {
         template.innerHTML = `
 
             <section>
+
                 <h2>
-                    <slot></slot>
+                    <slot name="title"></slot>
                 </h2>
+
+                <div>
+                    <p>
+                        <slot name="parrafo"></slot>
+                    </p>
+                </div>
+
             </section>
 
             ${this.getStyles()}
